e1643f54f767bf6368910804b3771f0c6eba01d1
= <=>
2023-02-04 08:05:00 +0100

    Client Parsing Input

--- Go_DB/Client/Client.go
+++ Go_DB/Client/Client.go
@@ -2,6 +2,7 @@ package Client
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -12,10 +13,43 @@ func getInput(reader *bufio.Reader) (string, error) {
 	return strings.TrimSpace(input), err
 }
 
+func parseInput(tokens []string) (string, error) {
+	command := tokens[0]
+	switch command {
+	case "help":
+		if len(tokens) != 1 {
+			return "", errors.New("help command does not take arguments")
+		}
+		return "Help Requested", nil
+	case "get":
+		if len(tokens) != 2 {
+			return "", errors.New("invalid arguments for get, see help")
+		}
+		key := tokens[1]
+		return fmt.Sprintf("Get Requested, Key = %v", key), nil
+	case "set":
+		if len(tokens) != 3 {
+			return "", errors.New("invalid arguments for set, see help")
+		}
+		key := tokens[1]
+		val := tokens[2]
+		return fmt.Sprintf("Set Requested, Key = %v, Val = %v", key, val), nil
+	default:
+		return "", errors.New("invalid command")
+	}
+}
+
 func Run() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		input, _ := getInput(reader)
-		fmt.Println(input)
+		tokens := strings.Fields(input)
+		if len(tokens) != 0 {
+			if output, err := parseInput(tokens); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(output)
+			}
+		}
 	}
 }
